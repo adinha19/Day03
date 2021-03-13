5b4c5bed617c967d7806405814c4302c4105a385
Adin Had <adinhadzovic1993@gmail.com>
2021-03-13 16:48:35 +0100

    mala ispravka

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,10 +1,12 @@
 function main() {
     // Only change code below this line
-var studlyCapVar = 10;
-var properCamelCase = "A String";
-var titleCaseOver = 9000;
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
 
-
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
     // Only change code above this line
     return {
 studlyCapVar,
